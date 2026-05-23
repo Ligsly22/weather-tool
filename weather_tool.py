8b9fa290cdc6567d55e6e94edb6c51f964e64241
Lars Liguori <Ligs22@outlook.com>
2026-05-23 17:48:13 +0200

    Add weather condition decoder

--- weather_tool.py
+++ weather_tool.py
@@ -28,6 +28,27 @@ weather_response = requests.get('https://api.open-meteo.com/v1/forecast', params
 weather_data = weather_response.json()
 current = weather_data['current_weather']
 
+weather_conditions = {
+    0: 'Clear sky',
+    1: 'Mainly clear',
+    2: 'Partly cloudy',
+    3: 'Overcast',
+    45: 'Foggy',
+    48: 'Icy fog',
+    51: 'Light drizzle',
+    61: 'Light rain',
+    63: 'Moderate rain',
+    65: 'Heavy rain',
+    80: 'Rain showers',
+    95: 'Thunderstorm',
+    99: 'Thunderstorm with hail'
+}
+
+code = current['weathercode']
+condition = weather_conditions[code]
+
+
 print(f"Weather in {city}: ")
+print(f"Conditions in {city} are: {weather_conditions[code]}")
 print(f"Temperature: {current['temperature']} C")
 print(f"Wind Speed: {current['windspeed']} Km/h")
