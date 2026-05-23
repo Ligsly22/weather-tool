808dfa8a379bbd440407eb64aee8f34cb55695ba
Lars Liguori <Ligs22@outlook.com>
2026-05-23 17:24:27 +0200

    Add weather tool - two API chain converting city name to live weather data

---
+++ weather_tool.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+import requests
+
+city = input("Enter a City: ")
+
+geo_params = {
+    'name': city,
+    'count': 1,
+}
+
+geo_requests = requests.get('https://geocoding-api.open-meteo.com/v1/search', params=geo_params)
+geo_data = geo_requests.json()
+# print(geo_data)
+
+location = geo_data['results'][0]
+lat = location['latitude']
+lon = location['longitude']
+# print(lat, lon)
+
+weather_params = {
+    'latitude': lat,
+    'longitude': lon,
+    'current_weather': True,
+}
+
+weather_response = requests.get('https://api.open-meteo.com/v1/forecast', params=weather_params)
+weather_data = weather_response.json()
+current = weather_data['current_weather']
+
+print(f"Weather in {city}: ")
+print(f"Temperature: {current['temperature']} C")
+print(f"Wind Speed: {current['windspeed']} Km/h")
